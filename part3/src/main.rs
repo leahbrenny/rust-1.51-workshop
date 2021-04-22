98f0e8fd174d71b0844ffd6c927d7b62bf557053
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2021-04-22 10:15:26 -0500

    fixed error with compiler. don't need to type in variable name Ex. residents:

--- part3/src/main.rs
+++ part3/src/main.rs
@@ -21,6 +21,22 @@ impl City {
                     residents,
                 )
             }
+            CitySize::City => {
+                let residents = 10_000;
+
+                (
+                    format!("a *city* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+            CitySize::Metropolis => {
+                let residents = 1_000_000;
+
+                (
+                    format!("a *metropolis* of approximately {} residents", residents),
+                    residents,
+                )
+            }
             // 👉 TODO Handle the other CitySize variants individually,
             //    in a similar way to how *town* is handled here
             _ => {
@@ -46,10 +62,7 @@ impl City {
 
 fn main() {
     // 👉 TODO Use City::new() to create a Metropolis-sized city here
-    let rustville = City {
-        description: String::new(),
-        residents: 0,
-    };
+    let rustville: City = City::new(CitySize::Metropolis, true);
 
     println!("This city is {}", rustville.description);
 
