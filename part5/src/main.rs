be3f7e8cd3523f7c9449231dff7c76bbf0b44c4d
Leah Brenny <leahbrenny@Leahs-MacBook-Pro.local>
2021-04-22 16:36:27 -0500

    finished part 5 changed functions no clone

--- part5/src/main.rs
+++ part5/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
 
-    let sum_of_nums = sum(numbers);
-    let product_of_nums = 0; // 👉 TODO call product() and put answer here
-    let average_of_nums = 0; // 👉 TODO call average() and put answer here
+    let (sum_of_nums , numbers2) = sum(numbers);
+    let (product_of_nums , numbers3) = product(numbers2); // 👉 TODO call product() and put answer here
+    let average_of_nums = average(numbers3); // 👉 TODO call average() and put answer here
 
     // 💡 TIP: You'll get a compile error. Here are two ways you can fix it:
     //
@@ -16,34 +16,34 @@ fn main() {
     //           you won't need to call .clone() at all!
     //
     // Give both options a try!
-
+ 
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 }
 
-fn sum(numbers: Vec<i64>) -> i64 {
+fn sum(numbers: Vec<i64>) -> (i64, Vec<i64>) {
     let mut total = 0;
 
     for num in numbers.iter() {
         total += num;
     }
 
-    total
+    (total, numbers)
 }
 
-fn product(numbers: Vec<i64>) -> i64 {
+fn product(numbers: Vec<i64>) -> (i64, Vec<i64>) {
     let mut total = 1;
 
     for num in numbers.iter() {
         total *= num;
     }
 
-    total
+    (total, numbers)
 }
 
 fn average(numbers: Vec<i64>) -> i64 {
     let length = numbers.len() as i64;
 
-    sum(numbers) / length
+    sum(numbers).0 / length
 }
